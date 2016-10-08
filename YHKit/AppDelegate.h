997a47fe68d135eeb920317ee76a7c00490a200d
yanghengzhan <yanghengzhan@sina.com>
2016-10-08 17:24:47 +0800

    Initial Commit

---
+++ YHKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YHKit
+//
+//  Created by yanghengzhan on 16/10/8.
+//  Copyright © 2016年 yanghengzhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YHKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YHKit
+//
+//  Created by yanghengzhan on 16/10/8.
+//  Copyright © 2016年 yanghengzhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
